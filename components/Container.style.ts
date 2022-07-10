527b26382df75e265f69c1af1f6e7f467bde821c
baekhyn <baekhyunhui@gmail.com>
2022-07-11 02:54:05 +0900

    💄 너비 변경

--- components/Container.style.ts
+++ components/Container.style.ts
@@ -1,16 +1,16 @@
-import styled from '@emotion/styled'
-import { css } from '@emotion/react'
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 const responsiveHeight = css`
     @media (max-width: 420px) {
         min-height: 600px;
     }
-`
+`;
 
 export const ContainerBox = styled.div`
-    max-width: 880px;
+    max-width: 800px;
     min-height: 700px;
     margin: 0.5rem auto;
     background-color: var(--main-white);
     ${responsiveHeight}
-`
+`;
