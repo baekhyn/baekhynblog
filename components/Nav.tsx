d2bcf97c6af7f98ce394e875990fc189a01ea0f3
baekhyn <baekhyunhui@gmail.com>
2022-09-10 20:32:02 +0900

    ♻️ Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,26 +1,26 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { ActiveNavItem, NavBox } from './Nav.style'
-import navdata from '../data/navdata'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { ActiveNavItem, NavBox } from './Nav.style';
+import navdata from '../data/navdata';
 
-export default function Nav() {
-    const router = useRouter()
-    const navPath = router.pathname
-    const navString = navPath.split('/')
+function Nav() {
+    const navPath = useRouter().pathname;
 
     return (
         <NavBox>
-            {navdata.map((navItem) => (
-                <span key={navItem.href}>
-                    <Link href={navItem.href}>
-                        {`/${navString[1]}` === navItem.href ? (
-                            <ActiveNavItem>{navItem.title}</ActiveNavItem>
+            {navdata.map((Item) => (
+                <span key={Item.href}>
+                    <Link href={Item.href}>
+                        {navPath.includes(Item.href) ? (
+                            <ActiveNavItem>{Item.title}</ActiveNavItem>
                         ) : (
-                            <a>{navItem.title}</a>
+                            <a>{Item.title}</a>
                         )}
                     </Link>
                 </span>
             ))}
         </NavBox>
-    )
+    );
 }
+
+export default Nav;
