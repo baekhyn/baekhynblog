3b2e4688bdd030bf09375377a234008a4011d99e
baekhyn <baekhyunhui@gmail.com>
2023-01-05 17:30:03 +0900

    Footer style 수정

--- components/Footer.style.ts
+++ components/Footer.style.ts
@@ -2,20 +2,16 @@ import styled from '@emotion/styled';
 
 export const FooterContainer = styled.footer`
     display: flex;
-    align-content: center;
-    flex: 1;
     flex-direction: column;
+    align-items: center;
     height: 100px;
     padding: 2rem 0;
     border-top: 1px solid var(--colors-gray4);
 
-    span,
-    a {
-        font-size: 0.7rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-top: auto;
-        height: 100px;
+    small,
+    p {
+        font-size: var(--font-size1);
+        color: var(--colors-black2);
+        margin: 0;
     }
 `;
