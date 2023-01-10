d3a93614ceb7b69a18e668742a34bdc8e555b056
baekhyn <baekhyunhui@gmail.com>
2023-01-10 16:36:06 +0900

    Banner 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import { getAllPostData } from '../util/post';
 import Container from '../components/Container';
 import Post from '../components/Post';
 import { PostType } from '../types/type';
-import Banner from '../components/Banner';
 import PageLink from '../components/PagiNation';
 
 const POST_LENGTH = 8;
@@ -11,7 +10,6 @@ const POST_LENGTH = 8;
 export default function Home({ allData }: { allData: Array<PostType> }) {
     return (
         <Container>
-            <Banner />
             <Post title={'Latest Posts'} length={POST_LENGTH} data={allData} />
             <PageLink />
         </Container>
